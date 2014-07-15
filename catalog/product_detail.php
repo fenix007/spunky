25f2767e859382b462a77f3fc071aeca2b1b533e
alex <o4kan@ya.ru>
2014-07-15 04:14:17 +0400

    product detail start

---
+++ catalog/product_detail.php
@@ -0,0 +1,308 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Карточка товара");
+?> 
+<script src="<?=SITE_TEMPLATE_PATH?>/js/jquery.aw-showcase.js"></script>
+ 
+<div class="content detail"> 
+<!-- content -->
+ <a href="#" class="button-back" >НАЗАД</a> 
+  <p class="breadcrumbs"> <a href="#" class="home" ><span></span><i></i></a> <a href="#" >Толстовки<i></i></a> <a href="#" >Chikana<i></i></a> <span>Mexico Chika (Raoul Roudrigez)</span> </p>
+ 
+  <div class="detail-item"> 
+    <div class="left-side"> 
+      <div class="tov-type"><i class="jacket"> 
+<!--.cap, .t-shirt, .jacket, .shoes-->
+ </i></div>
+     
+      <p class="price"><i></i>2.100 руб.</p>
+     
+      <div class="color"> <i></i> 
+        <div class="select"> 
+          <div class="sel-visible"> 
+            <div style="background-color: rgb(77, 255, 208);"></div>
+           </div>
+         <i class="trigger"></i> 
+          <div class="sel-list-wr"> 
+            <div class="col1"> 
+              <div class="co1"> <a href="#" class="item-col2" style="background-color: #ff0000;" ><i></i><input type="checkbox" /></a> <a href="#" class="item-col2" style="background-color: #ffed00;" ><i></i><input type="checkbox" /></a> <a href="#" class="item-col2" style="background-color: #9dff00;" ><i></i><input type="checkbox" /></a> <a href="#" class="item-col2" style="background-color: #009bff;" ><i></i><input type="checkbox" /></a> <a href="#" class="item-col2" style="background-color: #b1ff00;" ><i></i><input type="checkbox" /></a> <a href="#" class="item-col2" style="background-color: #00ffae;" ><i></i><input type="checkbox" /></a> </div>
+             
+              <div class="co2"> <a href="#" class="item-col2 selected" style="background-color: #ffed00;" ><i></i><input type="checkbox" checked="" /></a> <a href="#" class="item-col2" style="background-color: #9dff00;" ><i></i><input type="checkbox" /></a> <a href="#" class="item-col2" style="background-color: #009bff;" ><i></i><input type="checkbox" /></a> <a href="#" class="item-col2" style="background-color: #b1ff00;" ><i></i><input type="checkbox" /></a> </div>
+             </div>
+           
+            <div class="clr"></div>
+           </div>
+         </div>
+       </div>
+     
+      <div id="showcase" class="showcase"> 
+        <div class="showcase-slide"> 
+          <div class="showcase-content"> <img src="pic/item-slider/slide1.png" alt="01"  /> </div>
+         
+          <div class="showcase-thumbnail" style="background-image: url(http://spunky.local/catalog/pic/item-slider/slide1.png);"></div>
+         </div>
+       
+        <div class="showcase-slide"> 
+          <div class="showcase-content"> <img src="pic/item-slider/slide2.png" alt="02"  /> </div>
+         
+          <div class="showcase-thumbnail" style="background-image: url(http://spunky.local/catalog/pic/item-slider/slide2.png);"></div>
+         </div>
+       
+        <div class="showcase-slide"> 
+          <div class="showcase-content"> <img src="pic/item-slider/slide1.png" alt="01"  /> </div>
+         
+          <div class="showcase-thumbnail" style="background-image: url(http://spunky.local/catalog/pic/item-slider/slide1.png);"></div>
+         </div>
+       
+        <div class="showcase-slide"> 
+          <div class="showcase-content"> <img src="pic/item-slider/slide2.png" alt="02"  /> </div>
+         
+          <div class="showcase-thumbnail" style="background-image: url(http://spunky.local/catalog/pic/item-slider/slide2.png);"></div>
+         </div>
+       
+        <div class="showcase-slide"> 
+          <div class="showcase-content"> <img src="pic/item-slider/slide1.png" alt="01"  /> </div>
+         
+          <div class="showcase-thumbnail" style="background-image: url(http://spunky.local/catalog/pic/item-slider/slide1.png);"></div>
+         </div>
+       </div>
+     </div>
+   
+    <div class="right-side"> <a href="#" class="fav" ></a> 
+      <div class="share-wrap"> <a href="#" class="share" ></a> 
+        <div class="wrap-shared-popup"> 
+          <div class="shared-popup"> <i></i> <a href="#" class="ico1" ></a> <a href="#" class="ico2" ></a> <a href="#" class="ico3" ></a> <a href="#" class="ico4" ></a> <a href="#" class="ico5" ></a> <a href="#" class="ico6" ></a> <a href="#" class="ico7" ></a> </div>
+         </div>
+       </div>
+     
+      <p class="type">ТОЛСТОВКА CHIKANA </p>
+     
+      <p class="name">MEXICO CHIKA</p>
+     
+      <p class="by-who">by raoul roudrigez</p>
+     
+      <div class="sep-min"></div>
+     
+      <p class="text">Толстовка, из коллекции Chikana личной работы 
+        <br />
+       известного мексиканского дизайнера и члена 
+        <br />
+       картеля &laquo;ZazaParaza&raquo; &mdash; Рауля Радригеза </p>
+     
+      <div class="sep-min"></div>
+     
+      <div class="params"> 
+        <div class="select"> 
+          <div class="sel-visible"> 
+            <div class="color"> <i></i> 
+              <div style="background-color: rgb(77, 255, 208);"></div>
+             </div>
+           
+            <div class="size"> <i></i> <span><span>L</span> (50 cm)</span> </div>
+           <i class="trigger"></i> 
+            <div class="clr"></div>
+           </div>
+         
+          <div class="sel-list-wr"> 
+            <div class="col1"> 
+              <div class="co1"> <a href="#" class="item-col" style="background-color: #ff0000;" ><i></i><input type="checkbox" /></a> <a href="#" class="item-col" style="background-color: #ffed00;" ><i></i><input type="checkbox" /></a> <a href="#" class="item-col" style="background-color: #9dff00;" ><i></i><input type="checkbox" /></a> <a href="#" class="item-col" style="background-color: #009bff;" ><i></i><input type="checkbox" /></a> <a href="#" class="item-col" style="background-color: #b1ff00;" ><i></i><input type="checkbox" /></a> <a href="#" class="item-col" style="background-color: #00ffae;" ><i></i><input type="checkbox" /></a> </div>
+             
+              <div class="co2"> <a href="#" class="item-col selected" style="background-color: #ffed00;" ><i></i><input type="checkbox" checked="" /></a> <a href="#" class="item-col" style="background-color: #9dff00;" ><i></i><input type="checkbox" /></a> <a href="#" class="item-col" style="background-color: #009bff;" ><i></i><input type="checkbox" /></a> <a href="#" class="item-col" style="background-color: #b1ff00;" ><i></i><input type="checkbox" /></a> </div>
+             </div>
+           
+            <div class="col2"> 
+              <ul> 
+                <li> <a href="javascript:void(0)" > <span class="tp" data-sm="(50cm)">XS</span> <i class="isOffer is"></i> <span class="price">1.500 руб.</span> <input type="radio" name="rad1" value="XS" /> </a> </li>
+               
+                <li> <a href="javascript:void(0)" > <span class="tp" data-sm="(50cm)">S</span> <i class="isOffer"></i> <span class="price">1.500 руб.</span> <input type="radio" name="rad1" value="S" /> </a> </li>
+               
+                <li> <a href="javascript:void(0)" > <span class="tp" data-sm="(70cm)">M</span> <i class="isOffer"></i> <span class="price">1.500 руб.</span> <input type="radio" name="rad1" value="M" /> </a> </li>
+               
+                <li> <a href="javascript:void(0)" class="active" > <span class="tp" data-sm="(80cm)">L</span> <i class="isOffer"></i> <span class="price">1.500 руб.</span> <input type="radio" name="rad1" value="L" checked="" /> </a> </li>
+               
+                <li> <a href="javascript:void(0)" > <span class="tp" data-sm="(90cm)">XL</span> <i class="isOffer is"></i> <span class="price">1.500 руб.</span> <input type="radio" name="rad1" value="XL" /> </a> </li>
+               
+                <li> <a href="javascript:void(0)" > <span class="tp" data-sm="(100cm)">XXL</span> <i class="isOffer"></i> <span class="price">1.500 руб.</span> <input type="radio" name="rad1" value="XXL" /> </a> </li>
+               </ul>
+             </div>
+           
+            <div class="clr"></div>
+           </div>
+         </div>
+       </div>
+     
+      <div class="by"> 
+        <div class="in-order"><i></i>под заказ</div>
+       <a href="#" class="backet" ></a> <a href="#" class="get" >БЕРУ!</a> 
+        <div class="clr"></div>
+       </div>
+     
+      <div class="contain"> 
+        <p class="ttl">СОСТАВ:</p>
+       
+        <div class="col col1"> 
+          <p> <span class="sp1">Хлопок</span> <span class="bb"></span> <span class="sp2">80%</span> </p>
+         
+          <p> <span class="sp1">Хлопок</span> <span class="bb"></span> <span class="sp2">80%</span> </p>
+         </div>
+       
+        <div class="col col2"> 
+          <p> <span class="sp1">Хлопок</span> <span class="bb"></span> <span class="sp2">80%</span> </p>
+         
+          <p> <span class="sp1">Хлопок</span> <span class="bb"></span> <span class="sp2">80%</span> </p>
+         </div>
+       </div>
+     </div>
+   
+    <div class="clr"></div>
+   </div>
+ 
+  <p class="block-title"><i></i>ПОХОЖИЕ ВЕЩИ<i></i></p>
+ 
+  <div class="content-items"> 
+    <div class="item"> 
+      <div class="hover"> 
+        <div class="name">бейсболка «SPUNKY BOX» 
+          <br />
+         фирменная</div>
+       
+        <div class="border"></div>
+       
+        <div class="price2">2.100</div>
+       <a href="#" class="get" >БЕРУ!</a> <a href="#" class="detail" >Подробнее</a> <a href="#" class="fav" ></a> <a href="#" class="backet" ></a> 
+        <div class="share-wrap"> <a href="#" class="share" ></a> 
+          <div class="wrap-shared-popup"> 
+            <div class="shared-popup"> <i></i> <a href="#" class="ico1" ></a> <a href="#" class="ico2" ></a> <a href="#" class="ico3" ></a> <a href="#" class="ico4" ></a> <a href="#" class="ico5" ></a> <a href="#" class="ico6" ></a> <a href="#" class="ico7" ></a> </div>
+           </div>
+         </div>
+       </div>
+     <i class="ico label"></i> <img src="pic/tovars/tov1.png"  /> 
+      <div class="price"><i></i>2100 руб.</div>
+     </div>
+   
+    <div class="item"> 
+      <div class="hover"> 
+        <div class="name">бейсболка «SPUNKY BOX» 
+          <br />
+         фирменная</div>
+       
+        <div class="border"></div>
+       
+        <div class="price2">2.100</div>
+       <a href="#" class="get" >БЕРУ!</a> <a href="#" class="detail" >Подробнее</a> <a href="#" class="fav" ></a> <a href="#" class="backet" ></a> 
+        <div class="share-wrap"> <a href="#" class="share" ></a> 
+          <div class="wrap-shared-popup"> 
+            <div class="shared-popup"> <i></i> <a href="#" class="ico1" ></a> <a href="#" class="ico2" ></a> <a href="#" class="ico3" ></a> <a href="#" class="ico4" ></a> <a href="#" class="ico5" ></a> <a href="#" class="ico6" ></a> <a href="#" class="ico7" ></a> </div>
+           </div>
+         </div>
+       </div>
+     <i class="ico coffe"></i> <img src="pic/tovars/tov2.png"  /> 
+      <div class="price new-price"> 
+        <div class="old">2100 руб.</div>
+       
+        <div class="is">1200 руб.</div>
+       </div>
+     </div>
+   
+    <div class="item"> 
+      <div class="hover"> 
+        <div class="name">бейсболка «SPUNKY BOX» 
+          <br />
+         фирменная</div>
+       
+        <div class="border"></div>
+       
+        <div class="price2">2.100</div>
+       <a href="#" class="get" >БЕРУ!</a> <a href="#" class="detail" >Подробнее</a> <a href="#" class="fav" ></a> <a href="#" class="backet" ></a> 
+        <div class="share-wrap"> <a href="#" class="share" ></a> 
+          <div class="wrap-shared-popup"> 
+            <div class="shared-popup"> <i></i> <a href="#" class="ico1" ></a> <a href="#" class="ico2" ></a> <a href="#" class="ico3" ></a> <a href="#" class="ico4" ></a> <a href="#" class="ico5" ></a> <a href="#" class="ico6" ></a> <a href="#" class="ico7" ></a> </div>
+           </div>
+         </div>
+       </div>
+     <i class="ico coffe"></i> <img src="pic/tovars/tov1.png"  /> 
+      <div class="price hot"><i></i>2100 руб.</div>
+     </div>
+   
+    <div class="clr"></div>
+   <a href="#" class="more-similar" >Ещё похожих 24 вещи. Показать?</a> </div>
+ 
+  <div class="sep-min"></div>
+ 
+  <p class="about-brand-title">О бренде</p>
+ 
+  <div class="about-brand"> <img src="pic/man.png"  /> 
+    <div class="text-w"> 
+      <p class="name">raoul roudrigez</p>
+     
+      <p class="text">— is a sports clothing company located in Philadelphia, Pennsylvania. The company was established in 1904, and is the oldest sporting goods company in Philadelphia. By license agreements with Major League Baseball, the National Basketball Association, National Football League, National Hockey League, and the Collegiate Licensing Company, the company has been producing vintage sports goods such as jerseys, jackets, hats, and wool-felt historic pennants.</p>
+     </div>
+   
+    <div class="clr"></div>
+   </div>
+ 
+<!-- content -->
+ 
+  <div class="watched-items"> 
+    <div class="sep"></div>
+   
+    <p class="ttl-watched">просмотренные товары</p>
+   
+    <div class="swiper-container"> 
+      <div class="swiper-wrapper"> 
+        <div class="swiper-slide"> <a href="#" > <img src="pic/carousel-fav/fav1.png"  /> <span class="price"> <span class="in-backet"></span> 2.700 руб. </span> </a> </div>
+       
+        <div class="swiper-slide"> <a href="#" > <img src="pic/carousel-fav/fav2.png"  /> <span class="price"> 2.700 руб. </span> </a> </div>
+       
+        <div class="swiper-slide"> <a href="#" > <img src="pic/carousel-fav/fav1.png"  /> <span class="price"> 2.700 руб. </span> </a> </div>
+       
+        <div class="swiper-slide"> <a href="#" > <img src="pic/carousel-fav/fav2.png"  /> <span class="price"> <span class="in-backet"></span> 2.700 руб. </span> </a> </div>
+       
+        <div class="swiper-slide"> <a href="#" > <img src="pic/carousel-fav/fav1.png"  /> <span class="price"> 2.700 руб. </span> </a> </div>
+       
+        <div class="swiper-slide"> <a href="#" > <img src="pic/carousel-fav/fav2.png"  /> <span class="price"> <span class="in-backet"></span> 2.700 руб. </span> </a> </div>
+       
+        <div class="swiper-slide"> <a href="#" > <img src="pic/carousel-fav/fav1.png"  /> <span class="price"> 2.700 руб. </span> </a> </div>
+       
+        <div class="swiper-slide"> <a href="#" > <img src="pic/carousel-fav/fav2.png"  /> <span class="price"> 2.700 руб. </span> </a> </div>
+       
+        <div class="swiper-slide"> <a href="#" > <img src="pic/carousel-fav/fav1.png"  /> <span class="price"> 2.700 руб. </span> </a> </div>
+       
+        <div class="swiper-slide"> <a href="#" > <img src="pic/carousel-fav/fav2.png"  /> <span class="price"> 2.700 руб. </span> </a> </div>
+       
+        <div class="swiper-slide"> <a href="#" > <img src="pic/carousel-fav/fav1.png"  /> <span class="price"> 2.700 руб. </span> </a> </div>
+       
+        <div class="swiper-slide"> <a href="#" > <img src="pic/carousel-fav/fav2.png"  /> <span class="price"> 2.700 руб. </span> </a> </div>
+       
+        <div class="swiper-slide"> <a href="#" > <img src="pic/carousel-fav/fav1.png"  /> <span class="price"> 2.700 руб. </span> </a> </div>
+       
+        <div class="swiper-slide"> <a href="#" > <img src="pic/carousel-fav/fav2.png"  /> <span class="price"> 2.700 руб. </span> </a> </div>
+       
+        <div class="swiper-slide"> <a href="#" > <img src="pic/carousel-fav/fav1.png"  /> <span class="price"> 2.700 руб. </span> </a> </div>
+       
+        <div class="swiper-slide"> <a href="#" > <img src="pic/carousel-fav/fav2.png"  /> <span class="price"> 2.700 руб. </span> </a> </div>
+       
+        <div class="swiper-slide"> <a href="#" > <img src="pic/carousel-fav/fav1.png"  /> <span class="price"> 2.700 руб. </span> </a> </div>
+       
+        <div class="swiper-slide"> <a href="#" > <img src="pic/carousel-fav/fav2.png"  /> <span class="price"> 2.700 руб. </span> </a> </div>
+       
+        <div class="swiper-slide"> <a href="#" > <img src="pic/carousel-fav/fav1.png"  /> <span class="price"> 2.700 руб. </span> </a> </div>
+       
+        <div class="swiper-slide"> <a href="#" > <img src="pic/carousel-fav/fav2.png"  /> <span class="price"> 2.700 руб. </span> </a> </div>
+       
+        <div class="swiper-slide"> <a href="#" > <img src="pic/carousel-fav/fav1.png"  /> <span class="price"> 2.700 руб. </span> </a> </div>
+       
+        <div class="swiper-slide"> <a href="#" > <img src="pic/carousel-fav/fav2.png"  /> <span class="price"> 2.700 руб. </span> </a> </div>
+       </div>
+     
+      <div class="swiper-scrollbar"></div>
+     </div>
+   <a href="#" class="all-items" >Все просмотренные товары (94)</a> 
+    <div class="sep"></div>
+   </div>
+ 
+  <div class="upper-footer"> </div>
+ 
+  <div class="clr"></div>
+ </div>
+ <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ sect_header_top.php
@@ -0,0 +1,32 @@
+<a href="//<?=SITE_SERVER_NAME?>" class="col1" ><img src="<?=SITE_TEMPLATE_PATH?>/images/logo.png"  /></a> 
+<div class="col2"> <span class="style1">Есть вопросы? Звоните!</span> 
+  <br />
+ 
+  <br />
+ <span class="style2">8 800 200 404 5</span> 
+  <br />
+ <span class="style3">бесплатный звонок</span> 
+  <br />
+ 
+  <br />
+ <span class="style1">или задавайте online</span> 
+  <br />
+ <a href="#" class="style4" >Обратная связь</a> </div>
+ 
+<div class="col3 b-empty"> 
+<!-- удалить клас b-empty если корзина не пуста-->
+ 
+  <p class="empty">Твоя корзина ещё 
+    <br />
+   пуста. Давай 
+    <br />
+   выберем что-нибудь? )</p>
+ 
+  <div class="length"> 
+    <p class="title">Твоя корзина</p>
+   
+    <div class="count"> <span class="star-num">99</span> <span class="num">2</span> 
+      <p>Вещи</p>
+     </div>
+   </div>
+ <a href="#" class="go-basket" >На кассу<i></i></a> </div>
