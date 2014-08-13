6dd107f8c1084f9a66ad2a8828386cb1cb741937
alex <o4kan@ya.ru>
2014-08-13 18:45:27 +0400

    "add spunky templates system.auth.form system.auth.authorize system.auth.registration"

--- aaa.php
+++ aaa.php
@@ -1,6 +1,4 @@
 <?php   
-    $r1= getimagesize('http://zaikami.com/fotocollection/yagoda/model/model55_1.jpg');
-    $r2= getimagesize('http://softmg.ru/works/catalog48.jpg');
-    var_dump($r1, $r2);
+    echo phpinfo();
     //file_get_contents('http://www.biletolovilka.ru/rzd?from=%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0&to=%D0%9F%D0%B8%D1%82%D0%B5%D1%80&from1date=&from1time=&from2date=&from2time=&to1date=&to1time=&to2date=&to2time=&price=&cartype2=on&cartype3=on&email=o4kan%40ya.ru&num=&excl=&incl=&minPrice=&refreshNo=&refreshYes=&comment=&k=%D0%BA');
 ?>
