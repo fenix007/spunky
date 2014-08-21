358a5438eb7f077097ec2a1573be8d811e088761
alex <o4kan@ya.ru>
2014-08-21 11:16:28 +0400

    "change my_func formatItem"

--- bitrix/php_interface/include/functions/my_func.php
+++ bitrix/php_interface/include/functions/my_func.php
@@ -45,6 +45,64 @@ function formatItem($ITEM, $prices_class, $disclamers, $label_class)
    * $ITEM['COLOR_GROUPS']=>array(0 => array('name' => '', 'id' => '', 'colors' => array(), 'sizes' => array('price' => ...)))
    * $ITEM['COLORS']
    */
+  $COLOR_GROUPS     = array();
+  $COLOR_GROUPS_REF = array();
+  $COLORS           = array();
+  $SIZES            = array('american' => array(), 'clothes' => array());
+
+  if(isset($ITEM['OFFERS']))
+  {
+    foreach($ITEM['OFFERS'] as $OFFER):
+
+      //*********COLOR GROUPS************
+      $color_group_iblock_id = $OFFER['PROPERTIES']['COLOR_GROUP_REF']['LINK_IBLOCK_ID'];
+      $color_group_id        = $OFFER['PROPERTIES']['COLOR_GROUP_REF']['VALUE'];
+
+      if(isset($COLOR_GROUPS_REF[$color_group_id]))
+      {
+        $colors_cur = $COLOR_GROUPS_REF[$color_group_id];
+      }
+      else
+      {
+        $cg_res = CIBlockElement::GetByID($color_group_id);
+        if($ar_cg_res = $cg_res->getNext())
+        {
+          $color_group_name = $ar_cg_res['NAME'];
+        }
+
+        $colors_cur = getColors($color_group_iblock_id, $color_group_id);
+
+        $COLOR_GROUPS_REF[$color_group_id]  = $colors_cur;
+        $COLOR_GROUPS [$color_group_id]     = array(
+          'name'    => $color_group_name,
+          'colors'  => $colors_cur
+        );
+        foreach($colors_cur as $color_cur):
+          $COLORS[$color_cur['name']] = $color_cur['hex'];
+        endforeach;
+      }
+
+      //*********  SIZES  ************
+      $american_size     = $OFFER['PROPERTIES']['SIZES_AMERICAN']['VALUE'];
+      $clothes_size      = $OFFER['PROPERTIES']['SIZES_CLOTHES']['VALUE'];
+      $SIZES['american'] = array_merge($SIZES['american'], array_diff($SIZES['american'], $american_size));
+      $SIZES['clothes']  = array_merge($SIZES['clothes'], array_diff($SIZES['clothes'], $clothes_size));
+
+      $sizes_cur = array('american_size' => $american_size, 'clothes_size' => $clothes_size);
+
+      $sizes_cur = formatPrices(
+        $sizes_cur,
+        $OFFER['PRICES']['BASE']['DISCOUNT_VALUE'],
+        $OFFER['PRICES']['BASE']['DISCOUNT_DIFF_PERCENT'],
+        $OFFER['PRICES']['BASE']['VALUE_VAT'],
+        $prices_class,
+        $label_class,
+        $disclamers
+      );
+
+      $COLOR_GROUPS [$color_group_id]['sizes'] = $sizes_cur;
+    endforeach;
+  }
 
   $section_type = getSectionUfValue($ITEM['IBLOCK_ID'], $ITEM['IBLOCK_SECTION_ID'], 'UF_CLOTHES_TYPE');
   if($section_type)
@@ -52,21 +110,6 @@ function formatItem($ITEM, $prices_class, $disclamers, $label_class)
     $ITEM['SECTION_TYPE']  = $section_type['name'];
     $ITEM['SECTION_CLASS'] = $section_type['class'];
   }
-
-  //***calculate prices value***
-  if(isset($ITEM['OFFERS']) && isset($ITEM['OFFERS'][0]) && isset($ITEM['OFFERS'][0]['PRICES']['BASE']['VALUE_VAT']))
-  {
-    $ITEM['DISCOUNT_PRICE']  = $ITEM['OFFERS'][0]['PRICES']['BASE']['DISCOUNT_VALUE'];
-    $ITEM['DISCOUNT_PERC']   = $ITEM['OFFERS'][0]['PRICES']['BASE']['DISCOUNT_DIFF_PERCENT'];
-    $ITEM['PRICE']           = $ITEM['OFFERS'][0]['PRICES']['BASE']['VALUE_VAT'];
-  }
-  elseif(isset($ITEM['PRICES']['BASE']['VALUE_VAT']))
-  {
-    $ITEM['DISCOUNT_PRICE']  = $ITEM['PRICES']['BASE']['DISCOUNT_VALUE'];
-    $ITEM['DISCOUNT_PERC']   = $ITEM['PRICES']['BASE']['DISCOUNT_DIFF_PERCENT'];
-    $ITEM['PRICE']           = $ITEM['PRICES']['BASE']['VALUE_VAT'];
-  }
-  $ITEM['PRICE_POINT'] = pricePoint($ITEM['DISCOUNT_PRICE']);
 
   //***check prices type and class***
   if(isset($ITEM['PROPERTIES']['SALELEADER']) && trim($ITEM['PROPERTIES']['SALELEADER']['VALUE']))
@@ -78,21 +121,62 @@ function formatItem($ITEM, $prices_class, $disclamers, $label_class)
     $ITEM['PRICE_TYPE'][]   = 'SPECIALOFFER';
   }
 
-  if($ITEM['DISCOUNT_PRICE'] !== $ITEM['PRICE'])
-  {
-    $ITEM['PRICE_TYPE'][]   = 'DISCOUNT';
-  }
-
-  $ITEM['PRICE_CLASS'] = formatArrayClasses($ITEM['PRICE_TYPE'], $prices_class);
-
-
   //***check labels type and class***
   if(isset($ITEM['PROPERTIES']['NEWPRODUCT']) && trim($ITEM['PROPERTIES']['NEWPRODUCT']['VALUE']))
   {
     $ITEM['LABEL_TYPE'][] = 'NEWPRODUCT';
   }
 
+  //***calculate prices value***
+  if(isset($ITEM['OFFERS']) && isset($ITEM['OFFERS'][0]) && isset($ITEM['OFFERS'][0]['PRICES']['BASE']['VALUE_VAT']))
+  {
+    $ITEM = formatPrices(
+      $ITEM,
+      $ITEM['OFFERS'][0]['PRICES']['BASE']['DISCOUNT_VALUE'],
+      $ITEM['OFFERS'][0]['PRICES']['BASE']['DISCOUNT_DIFF_PERCENT'],
+      $ITEM['OFFERS'][0]['PRICES']['BASE']['VALUE_VAT'],
+      $prices_class,
+      $label_class,
+      $disclamers
+    );
+  }
+  elseif(isset($ITEM['PRICES']['BASE']['VALUE_VAT']))
+  {
+    $ITEM = formatPrices(
+      $ITEM,
+      $ITEM['PRICES']['BASE']['DISCOUNT_VALUE'],
+      $ITEM['PRICES']['BASE']['DISCOUNT_DIFF_PERCENT'],
+      $ITEM['PRICES']['BASE']['VALUE_VAT'],
+      $prices_class,
+      $label_class,
+      $disclamers
+    );
+  }
+
+  return $ITEM;
+}
+
+function formatPrices($ITEM, $discount_price, $percent, $price, $prices_class, $label_class, $disclamers)
+{
+  $ITEM['DISCOUNT_PRICE']  = $discount_price;
+  $ITEM['DISCOUNT_PERC']   = $percent;
+  $ITEM['PRICE']           = $price;
+
+  if($ITEM['DISCOUNT_PRICE'] !== $ITEM['PRICE'])
+  {
+    $ITEM['PRICE_TYPE'][]   = 'DISCOUNT';
+  }
+
+  $ITEM['PRICE_POINT'] = pricePoint($ITEM['DISCOUNT_PRICE']);
+
+  $ITEM['PRICE_CLASS'] = formatArrayClasses($ITEM['PRICE_TYPE'], $prices_class);
+
   $ITEM['LABEL_CLASS'] = formatArrayClasses($ITEM['LABEL_TYPE'], $label_class);
+
+  if(!isset($ITEM['LABEL_TYPE']))
+  {
+    $ITEM['LABEL_TYPE'] = array();
+  }
 
   $ITEM['TYPES'] = array_merge($ITEM['PRICE_TYPE'], $ITEM['LABEL_TYPE']);
 
@@ -109,6 +193,31 @@ function formatItem($ITEM, $prices_class, $disclamers, $label_class)
   endforeach;
 
   return $ITEM;
+}
+
+function getColors($color_group_iblock_id, $color_group_id)
+{
+  $arFilter = Array("IBLOCK_ID"=>$color_group_iblock_id, "ID"=>$color_group_id);
+  $res      = CIBlockElement::GetList(array(), $arFilter, false, false, array('PROPERTY_colors', 'PROPERTY_preview'));
+
+  $ress = false;
+
+  while($ar_res = $res->GetNext())
+  {
+    $sub_arFilter = Array("ID"=>$ar_res['PROPERTY_COLORS_VALUE']);
+    $sub_res      = CIBlockElement::GetList(array(), $sub_arFilter, false, false, array('ID', 'NAME', 'PROPERTY_code'));
+    //$sub_res = CIBlockElement::GetByID($ar_res['PROPERTY_COLORS_VALUE']);
+    if($ar_sub_res = $sub_res->GetNext())
+    {
+      $ress[] = array(
+        'id'   => $ar_sub_res['ID'],
+        'name' => $ar_sub_res['NAME'],
+        'hex'  => $ar_sub_res['PROPERTY_CODE_VALUE'],
+      );
+    }
+  }
+
+  return $ress;
 }
 
 function getSectionUfValue($iblock_id, $section_id, $uf_code)
@@ -137,7 +246,7 @@ function getSectionUfValue($iblock_id, $section_id, $uf_code)
 }
 
 /**
- *Функция форматирует цену, разделя триады точкой:
+ *Функция форматирует цену, разделяя триады точкой:
  *  9000212=> 9.000.212
  *
  * @param int $price
